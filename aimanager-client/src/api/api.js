5db8eec077177d9917d288bc74ebb444a894f5cd
danielcuervox <danielcuervox@hotmail.com>
2026-07-31 20:41:10 +0200

    token intersection is improved

--- aimanager-client/src/api/api.js
+++ aimanager-client/src/api/api.js
@@ -18,4 +18,28 @@ api.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
+api.interceptors.response.use(
+  (response) => {
+    // Si todo va bien (Status 200, 201, etc.), simplemente dejamos pasar la respuesta.
+    return response;
+  },
+  (error) => {
+    // Si el backend nos devuelve un error, se analiza aquí.
+    if (error.response && error.response.status === 401) {
+      console.warn(
+        "Sesión expirada o token inválido. Redirigiendo al login...",
+      );
+
+      // se borra el toquen caducado o inválido
+      localStorage.removeItem("token");
+
+      // se redirige al usuario a la pantalla de login.
+      // se usa window.location.href porque useNavigate() solo funciona dentro de componentes React.
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default api;
