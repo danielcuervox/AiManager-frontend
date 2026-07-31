99df3b4a0e5c7baea725365dae84085d32b2511a
danielcuervox <danielcuervox@hotmail.com>
2026-07-31 23:53:03 +0200

    complete login with google

--- aimanager-client/src/App.jsx
+++ aimanager-client/src/App.jsx
@@ -5,23 +5,26 @@ import { Register } from "./pages/Register";
 import { Activities } from "./pages/Activities";
 import "./App.css";
 import { LanguageProvider } from "./context/LanguageContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   return (
-    <LanguageProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+    <GoogleOAuthProvider clientId="658709391781-jlaqf3iqgs0uo3qphtgg65mk3vkohlk7.apps.googleusercontent.com">
+      <LanguageProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-          {/* si no está logueado */}
-          <Route path="/" element={<Navigate to="/login" />} />
+            {/* si no está logueado */}
+            <Route path="/" element={<Navigate to="/login" />} />
 
-          {/* panel principal */}
-          <Route path="/activities" element={<Activities />} />
-        </Routes>
-      </BrowserRouter>
-    </LanguageProvider>
+            {/* panel principal */}
+            <Route path="/activities" element={<Activities />} />
+          </Routes>
+        </BrowserRouter>
+      </LanguageProvider>
+    </GoogleOAuthProvider>
   );
 }
 
--- aimanager-client/src/pages/Login.jsx
+++ aimanager-client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import api from "../api/api";
 import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
@@ -17,8 +18,29 @@ export const Login = () => {
 
       navigate("/activities");
     } catch (err) {
-      setError("Credenciales incorrectas. Inténtalo de nuevo.");
+      setError("Credenciales incorrectas. Inténtalo de nuevo." + err);
     }
+  };
+
+  const handleGoogleSuccess = async (credentialResponse) => {
+    const googleToken = credentialResponse.credential;
+    console.log("Token de Google recibido, enviando al backend...");
+
+    try {
+      const response = await api.post("/api/auth/google", {
+        token: googleToken,
+      });
+      localStorage.setItem("token", response.data);
+      navigate("/activities");
+    } catch (err) {
+      console.error("Error validando token en el backend", err);
+      setError("Error al iniciar sesión con Google.");
+    }
+  };
+
+  const handleGoogleError = () => {
+    console.log("El inicio de sesión con Google falló");
+    setError("Error al conectar con Google.");
   };
 
   const handleRegisterRedirect = () => {
@@ -58,8 +80,25 @@ export const Login = () => {
             className="bg-green-600 hover:bg-green-700 p-3 rounded font-bold transition"
             onClick={handleRegisterRedirect}
           >
-            Registrarse
+            Registrarse22
           </button>
+          {/* Separador  */}
+          <div className="flex items-center my-2">
+            <div className="flex-grow border-t border-slate-700"></div>
+            <span className="px-3 text-slate-400 text-sm">O</span>
+            <div className="flex-grow border-t border-slate-700"></div>
+          </div>
+
+          {/* 2. Botón de inicio de sesión de Google */}
+          <div className="flex justify-center">
+            <GoogleLogin
+              onSuccess={handleGoogleSuccess}
+              onError={handleGoogleError}
+              theme="filled_black"
+              shape="rectangular"
+              text="signin_with"
+            />
+          </div>
         </form>
       </div>
     </div>
